505d167e0f633f2c62046c05b4b145bd4942c81b
Zach LeClaire <zjl16@case.edu>
2022-02-16 00:18:42 -0500

    resolved bugs with indexing

--- clustering.py
+++ clustering.py
@@ -24,7 +24,7 @@ class clustering:
 
         #initialization:
         #number of vectors
-        MAX_THRESH = 10
+        MAX_THRESH = 100
         n = D.shape[0]
         m = D.shape[1]
 
@@ -37,14 +37,12 @@ class clustering:
         
         #q_array = np.zeros(1,int(n))
         while(abs(delta_Q) > tau and time < MAX_THRESH):
-            #first, we update the cluster centers
+            #first, we update the cluster mediods
             for k in range(0, cluster_count):
                 #select indices in cluster k
-                indices_k = []
-                for i in range(0, n):
-                    if(Identity[i] == cluster_count):
-                        indices_k.append(i)
+                indices_k = clustering.get_cluster(Identity, k, n)
                 k_count = len(indices_k)
+                #print(indices_k)
                 if(k_count > 0):
                     # center of the data
                     center_dist = float("inf")
@@ -57,6 +55,7 @@ class clustering:
                         if(total_dist < center_dist):
                             center_index = i
                             center_dist = total_dist
+                        #print(f"total distance: {total_dist}")
                     #print(k, indices_k)
                     #print(f"Indices: {len(indices_k)} Identity_center: {Identity_center.shape} k: {k} center: {center_index}")
                     Identity_center[k] = indices_k[center_index]
@@ -69,18 +68,17 @@ class clustering:
                 min_index = -1
                 for j in range(0, cluster_count):
                     dist = D[i, Identity_center[j]]
-                    print(j, dist)
+                    #print(j, dist)
                     if dist < min:
                         min = dist
                         min_index = j
-                print(min_index)
                 Identity[i] = min_index
-            print(Identity)
+            print(Identity_center)
             
             #update tightness
             tigtness_current = 0
             for i in range(1, n):
-                tigtness_current += D[i, Identity_center[Identity[i]]]**2
+                tigtness_current += D[i, Identity_center[Identity[i]]]
             delta_Q = tigtness_current - tightness_last
             tightness_last = tigtness_current
             #update time step
@@ -101,9 +99,69 @@ class clustering:
         return clustering.k_mediods(k, D, tau)
     
     @staticmethod
-    def k_means(k, X, tau):
-        print("MeMe")
+    def k_means(cluster_count, X, tau):
+        if(cluster_count < 1 or tau < 0):
+            print("Invalid Inputs")
+        #initialization:
+        #number of vectors
+        MAX_THRESH = 100
+        n = X.shape[0]
+        m = X.shape[1]
 
+        tigtness_current = 0
+        tightness_last = 0
+        delta_Q = float("inf")
+        time = 0
+        Identity = np.random.randint(cluster_count, size=n) # each index has value \in {1...k}
+        char_vecs = []  # characteristic vectors
+        print(Identity)
+        
+        #itterate until threshold
+        while(abs(delta_Q) > tau and time < MAX_THRESH):
+            #print(char_vecs)
+            #first, we update the characteristic vectors
+            char_vecs = np.zeros((cluster_count,m))
+            for k in range(0, cluster_count):
+                indices_k = clustering.get_cluster(Identity, k, n)
+                k_count = len(indices_k)
+                if(k_count != 0):
+                    for j in range(0, k_count):
+                        char_vecs[k] += X[j]
+                    char_vecs[k] = 1/k_count*char_vecs[k]
+
+            #second, assign each vector into a clustering
+            for i in range(0, n):
+                min = float("inf")
+                min_index = -1
+                for j in range(0, cluster_count):
+                    dist = clustering.euclidian_distance(X[i], char_vecs[j])
+                    #print(j, dist)
+                    if dist < min:
+                        min = dist
+                        min_index = j
+                Identity[i] = min_index
+            
+            #update tightness
+            tigtness_current = 0
+            for i in range(1, n):
+                tigtness_current += clustering.euclidian_distance(X[i], char_vecs[Identity[i]])
+            delta_Q = tigtness_current - tightness_last
+            tightness_last = tigtness_current
+            #update time step
+            time += 1
+            print(time, delta_Q)
+        print(char_vecs)
+        
+        return Identity
+
+
+    @staticmethod
+    def get_cluster(Identity, k, n):
+        indices_k = []
+        for i in range(0, n):
+            if(Identity[i] == k):
+                indices_k.append(i)
+        return indices_k
 
     @staticmethod
     def is_normal_arr(a): # a is input array to be tested
@@ -111,7 +169,7 @@ class clustering:
 
     @staticmethod
     def euclidian_distance(x1,x2):
-        return np.linalg.norm(x1-x2)
+        return abs(np.linalg.norm(x1-x2))
 
     #non-convergent algorithms anyone?
     @staticmethod
